1984f2d2a0c350d426318b17f765f7052929753a
Guruprasad <guruprasad@nichesolv.com>
2023-04-07 13:52:30 +0530

    Initial commit

--- src/main/java/com/example/test/demo/DemoApplication.java
+++ src/main/java/com/example/test/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 @Slf4j
 public class DemoApplication {
 
