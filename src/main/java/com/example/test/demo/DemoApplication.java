79448035486a7f53903741e38c911e4eba2054a1
Guruprasad <guruprasad@nichesolv.com>
2023-04-07 13:07:47 +0530

    Initial commit

---
+++ src/main/java/com/example/test/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.test.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping
+    public void logTesting() {
+        log.info("THi sis a testing log");
+		log.info("2nd log for testing");
+    }
+
+}
